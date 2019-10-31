c5788f5acd2734ade8cef994a002412d352d6c43
Alex Studer <alex@studer.dev>
2019-10-30 21:37:03 -0400

    clean up user detection thingy, make api module more consistent with the client's version

--- js/page.js
+++ js/page.js
@@ -1,17 +1,19 @@
 api.onReady(function() {
-    api.GET("/auth/me", {}, function(data) {
-        if(data.status == "ok") {
-            //the user is logged in
-            $("#uname").text(data.name.split(" ")[0])
-            $("#logout").click(function() {
-                api.GET("/auth/logout", {}, function() {
-                    location.reload()
-                });
-            })
-            $("#log-in-options").hide();
-            $("#log-out-options").removeClass("hidden");
-            var helloWords = ["Hey", "Hi", "Hello", "Howdy", "What's up"]
-            $(".hello-word").text(helloWords[Math.floor(Math.random() * helloWords.length)]);
-        }
-    })
-})
+	api.get("auth/me", {}, function(data) {
+		if (data.status == "ok") {
+			$("#user-name").text(data.name);
+			$("#log-in-options").hide();
+			$("#log-out-options").removeClass("hidden");
+			var helloWords = ["Hey", "Hi", "Hello", "Howdy", "What's up"];
+			$(".hello-word").text(helloWords[Math.floor(Math.random() * helloWords.length)]);
+		}
+	});
+});
+
+window.addEventListener("load", function() {
+	document.getElementById("logout").addEventListener("click", function() {
+		api.get("auth/logout", {}, function() {
+			window.location.reload();
+		});
+	});
+});
